390e8733ddba9faf0a6a668d74ccecdc7aeb8375
srivathsan <srivathsan92@gmail.com>
2015-04-12 12:30:41 +0200

    changed image normalization stuff to make it better, subsamples and uses canny filter

--- imageNormalizatin.m
+++ imageNormalizatin.m
@@ -2,15 +2,12 @@ function [ outputFrame ] = imageNormalizatin( inputFrame )
 %imageNormalization frame normalization and image noise correction
     % convert to gray scale
     grayFrame = rgb2gray(inputFrame);
-    
-    %median filter
-    medianFrame = medfilt2(grayFrame);
-
-    % sharpen image
-    sharpenedImage = imsharpen(medianFrame);
-
+    grayFrame = grayFrame(1:2:end,1:2:end);
+    grayFrame = medfilt2(grayFrame);
     % edge detection
-    outputFrame = edge(sharpenedImage,'Prewitt');
-    
+    canny_thresh = [0.1 0.2]; % you might need to
+    canny_sigma = 5; % change these for your image
+    outputFrame = edge(grayFrame,'canny',canny_thresh,canny_sigma);
+       
 end
 
